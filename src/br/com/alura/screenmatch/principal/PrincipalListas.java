c2d1958689bcfc4a6637a449fde73bb28ebdf60c
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-11 17:55:44 -0300

    Outros metodos de ordenação

--- src/br/com/alura/screenmatch/principal/PrincipalListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalListas.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.principal;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
@@ -18,9 +20,9 @@ public class PrincipalListas {
 
         ArrayList<Titulo> listaDeFilmes = new ArrayList<>();
 
+        listaDeFilmes.add(novoFilme);
         listaDeFilmes.add(outroFilme);
         listaDeFilmes.add(meuFilme);
-        listaDeFilmes.add(novoFilme);
 
         Serie minhaSerie = new Serie("Game of Thrones", 2011, false, 60, 8, 10, 60, true);
 
@@ -38,5 +40,30 @@ public class PrincipalListas {
                 System.out.println(" Classificação: " + filme.getClassificacao());
                 }
         }
+
+        System.out.println("\n*************************************************************");
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(listaDeFilmes);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        for (Titulo titulo : listaDeFilmes) {
+            // System.out.println(listaDeFilmes.get(i).getNome());
+            System.out.print(titulo);
+            if (titulo instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println(" Classificação: " + filme.getClassificacao());
+                }
+        };
+
+        listaDeFilmes.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println(listaDeFilmes);
     }
 }
